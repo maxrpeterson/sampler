b976be585ed768f8970782fdd76db8b8de053d2f
Max Peterson <petersonmaxr@gmail.com>
2020-03-02 00:27:28 -0500

    Adds the Tone player stuff for demo purposes
    There seems to be some noticeable input lag, I need to experiment with
    implementing using vanilla WebAudio api to see if it speeds it up any

--- app/index.js
+++ app/index.js
@@ -1,3 +1,36 @@
-import * as Tone from './Tone.js'
+/* global Tone */
+import './Tone.js'
 
-console.log('hello from index')
+const kickPlayer = new Tone.Player('samples/DX Bass 8 Med.wav').toMaster()
+const clapPlayer = new Tone.Player('samples/DX Clap 16.wav').toMaster()
+console.log(kickPlayer)
+
+const kick = document.querySelector('.js-pad-kick')
+const clap = document.querySelector('.js-pad-clap')
+
+console.log(kick)
+
+function startPlayer(player) {
+  if (player.state === 'started') {
+    player.restart()
+  } else {
+    player.start()
+  }
+}
+
+function play(player) {
+  return (e) => {
+    e.preventDefault()
+    if (Tone.context.state !== 'running') {
+      Tone.start()
+        .then(() => startPlayer(player))
+    } else {
+      startPlayer(player)
+    }
+  }
+}
+
+kick.addEventListener('touchstart', play(kickPlayer))
+kick.addEventListener('mousedown', play(kickPlayer))
+clap.addEventListener('touchstart', play(clapPlayer))
+clap.addEventListener('mousedown', play(clapPlayer))
